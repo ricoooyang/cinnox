4d8f2ecc2fe2114b8d050605e93af90d10e8cf07
rico <a0955383566@gmail.com>
2023-02-08 18:40:45 +0800

    setup a Sprint Boot project

---
+++ src/main/java/cinnox/CinnoxApplication.java
@@ -0,0 +1,13 @@
+package cinnox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinnoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinnoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cinnox/CinnoxApplicationTests.java
@@ -0,0 +1,13 @@
+package cinnox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinnoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
